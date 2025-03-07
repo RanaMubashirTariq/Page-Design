18954277a43074ebccd4e8c46152f1860bcc701a
Rana Mubashir Tariq <mubashirtariq.1366@gmail.com>
2025-03-08 03:27:33 +0500

    add files

--- src/components/SignUpSection.js
+++ src/components/SignUpSection.js
@@ -4,7 +4,7 @@ import './SignUpSection.css';
 export default function SignUpSection() {
   return (
     <div className='sign-up-main-div'>
-             <div className='sign-up-back-img'><img src="/Pictures/signup-back-pic.svg"/></div>
+             <img src="/Pictures/signup-back-pic.svg"/>
     </div>
   )
 }
